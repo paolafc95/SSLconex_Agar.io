3fa7c40db3f80478867cf4be662d331c0df0da1c
Paola Fuentes Caro <paolafc95@gmail.com>
2019-03-29 18:24:27 -0500

    ClienteSSL done

--- src/conexionssl/ClienteSSL.java
+++ src/conexionssl/ClienteSSL.java
@@ -9,7 +9,7 @@ import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
 public class ClienteSSL {
-    public static void main (String[] args){
+    public static void main (String[] args) throws ClassNotFoundException{
         System.setProperty("java.net.ssl.trustore", "./resources/Servidor.jks" );
         ObjectOutputStream salida = null;
         ObjectInputStream entrada = null;
@@ -18,12 +18,40 @@ public class ClienteSSL {
         try{
             SSLSocketFactory fact = (SSLSocketFactory) SSLSocketFactory.getDefault();
             sslsocket = (SSLSocket) fact.createSocket("localhost", 8000);
+            //hacer handshake
+            sslsocket.startHandshake();
+            System.out.println("Se ha autenticado correctamente");
             
+            salida = new ObjectOutputStream(sslsocket.getOutputStream());
+            entrada = new ObjectInputStream(sslsocket.getInputStream());
+            
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+            boolean exit = false;
+            while (!exit){
+                System.out.println("> ");
+                String line = buffer.readLine();
+                salida.writeObject(line);
+                salida.flush();
+                
+                String st = (String) entrada.readObject();
+                System.out.println(st);
+            }         
+        
         }catch (IOException ex) {
+            } catch (ClassNotFoundException ex) {
+
+		} finally {
+
+			try {
+				entrada.close();
+				sslsocket.close();
+			} catch (IOException ex) {
+
+			}
+		}
+            
 
 		}
         
         
     }
-    
-}
